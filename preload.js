fe3bfb308d1b58044d3dee8fe850ee4807d5ab11
Dasko2 <david.azerty1234567890@gmail.com>
2026-06-12 14:33:03 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,16 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('launcher', {
+
   launch: (d) => ipcRenderer.invoke('launch:game', d),
+
   listMods: () => ipcRenderer.invoke('mods:list'),
   openModsFolder: () => ipcRenderer.invoke('mods:open-folder'),
   getInfo: () => ipcRenderer.invoke('app:info'),
 
-  onLog: (cb) => {
-    ipcRenderer.on('log', (_, d) => cb(d));
-  },
-
-  onProgress: (cb) => {
-    ipcRenderer.on('progress', (_, d) => cb(d));
-  }
+  onLog: (cb) => ipcRenderer.on('log', (_, d) => cb(d)),
+  onProgress: (cb) => ipcRenderer.on('progress', (_, d) => cb(d))
 });
